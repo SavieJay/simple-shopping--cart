a2d642ba24775660fdefdd38ed594d6699da6f01
Ralpmike <ralphmike996@gmail.com>
2024-09-12 16:07:34 +0100

    feat: build checkout page and logic implementation

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 // Your existing variables
 let products = document.querySelector("#products");
+let overLay = document.querySelector(".overlay");
 let product = document.querySelectorAll(".product");
-let total = document.querySelector(".total");
 let allProducts = [];
 let cartState = []; // Track cart state for each product
 let allCartProducts = document.querySelector(".cart-header");
 let numOfCartItems = document.querySelector(".num-of-items");
+let neworderConfirmation = document.querySelector(".confirm-items");
+
+let confirmTotal = document.querySelector(".order-total > span");
+let emptyCart = document.querySelector(".empty-cart");
+let totalCost = document.querySelector(".totalCost");
+console.log(cartState);
 
 // Your existing cartproducts function
 
@@ -15,26 +21,26 @@ function cartproducts() {
   cartState.map((item) => {
     console.log(item);
     allCartProducts.innerHTML += `
-    <div class="cart-header">
+  <div class="pricing-container">
+    <div class="cart-pricing">
       <h3>${item?.category}</h3>
-      <div>
         <div class='pricing'>
           <span class="amount">${item?.quantity}x</span>
-          <span class="price">@$${item?.price}</span>
+          <span class="price">@$${item?.price.toFixed(2)}</span>
           <span class="total">$${(item?.price * item?.quantity).toFixed(
             2
           )}</span>
         </div>
+    </div>
 
-        <button id="remove-item" onclick="removeFromCart(${item?.id})">
+      <button id="remove-item" onclick="removeFromCart(${item?.id})">
          <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
          width="24" height="24" fill="none" viewBox="0 0 24 24">
          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
            d="m15 9-6 6m0-6 6 6m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
            </svg>
-        </button>
-      </div>
-    </div>
+      </button>
+  </div>
     `;
   });
 }
@@ -62,18 +68,25 @@ function displayProduct() {
     div.setAttribute("data-key", item.id);
 
     const productInCart = cartState.find((product) => product?.id == item.id);
-    console.log({ productInCart });
     // Conditionally render either "Add to Cart" button or quantity controls
     div.innerHTML = `
       <div class="item">
-        <img src=${item.image.desktop} alt=${item.name}>
+        <img src=${
+          window.innerWidth < 768 ? item.image.mobile : item.image.desktop
+        } alt=${item.name} style= "border: ${
+      productInCart ? "3px solid #D87D4A" : "none"
+    }";>
       </div>
       ${
         productInCart
           ? incrementCartButton(item.id) // Show quantity controls if item is in cart
           : addToCartButton(item.id) // Show "Add to Cart" if item not in cart
       }
-      <i>${item.name}</i>
+      <div class="info">
+      <p>${item?.category}</p>
+      <p>${item?.name}</p>
+      <p>$${item?.price}</p>
+      </div>
     `;
     products.appendChild(div);
   });
@@ -83,7 +96,8 @@ function displayProduct() {
 function addToCartButton(id) {
   return `
     <button class="add-to-cart" data-key=${id}>
-      <p>Add to cart</p>
+      <img src="./assets/images/icon-add-to-cart.svg" alt="">
+      <p class="text">Add to cart</p>
     </button>
   `;
 }
@@ -92,17 +106,18 @@ function addToCartButton(id) {
 function incrementCartButton(id) {
   const product = cartState.find((product) => product?.id == id);
   const quantity = product?.quantity || 1; // Ensure quantity is not undefined
+  // product.image.style.border = "2px solid #D87D4A";
   return `
     <div class="quantity add-to-cart">
       <button onclick="decrementQuantity(${id})">
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-minus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-minus" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
           <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
           <line x1="5" y1="12" x2="19" y2="12" />
         </svg>
       </button>
       <span>${quantity}</span>
       <button onclick="incrementQuantity(${id})">
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
           <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
           <line x1="12" y1="5" x2="12" y2="19" />
           <line x1="5" y1="12" x2="19" y2="12" />
@@ -130,14 +145,24 @@ products.addEventListener("click", (event) => {
     cartState.push({ ...product, quantity: 1 });
   }
 
+  if (cartState.length > 0) {
+    emptyCart.style.display = "none";
+    calculateTotal();
+  } else {
+    emptyCart.style.display = "block";
+  }
+
   // Update the cart UI and product list UI
   updateCartUI();
+  calculateTotal();
   console.log({ cartState });
 });
 
 function updateCartUI() {
   numOfCartItems.innerHTML =
-    cartState.length >= 1 ? `Your Cart (${cartState.length})` : "Your Cart (0)";
+    cartState.length >= 1
+      ? `Your Cart (${quantityOfItemsInCart()})`
+      : "Your Cart (0)";
   cartproducts();
   displayProduct(); // Update only the cart display
 }
@@ -149,7 +174,11 @@ function removeFromCart(id) {
   const item = cartState.findIndex((product) => product.id == id);
 
   cartState.splice(item, 1);
+  if (!item && !cartState.length > 0) {
+    emptyCart.style.display = "block";
+  }
   updateCartUI();
+  calculateTotal();
 }
 
 // Function to increment quantity
@@ -181,4 +210,80 @@ function decrementQuantity(id) {
     // Update the cart UI and product list UI
     updateCartUI(); // Refresh product UI to show the updated button
   }
+}
+
+function calculateTotal() {
+  totalCost.innerHTML = cartState.length > 0 ? `${confirmOrder()}` : "";
+
+  updateCartUI();
+}
+
+function quantityOfItemsInCart() {
+  return cartState.reduce((total, item) => total + item.quantity, 0);
+}
+
+function confirmOrder() {
+  let total = 0;
+  cartState.forEach((item) => {
+    total += item.price * item.quantity;
+  });
+  confirmTotal.innerHTML = `$${total.toFixed(2)}`;
+  return `
+  <div class="order-total">
+  <p>Order Total</p> 
+  <span id="totalcost">$${total.toFixed(2)}</span>  
+  </div>
+  
+  <div class="carbon-free">
+ <img src="./assets/images/icon-carbon-neutral.svg" alt="carbon">
+ <p>This is a <span>carbon-neutral</span> delivery</p>
+  </div>
+  
+  <button class="confirm-order" onclick="placeOrder()">
+    Confirm Order
+  </button>
+  `;
+}
+
+function placeOrder() {
+  neworder();
+  overLay.style.display = "block";
+}
+
+function neworder() {
+  neworderConfirmation.innerHTML = "";
+
+  cartState.forEach((item) => {
+    neworderConfirmation.insertAdjacentHTML(
+      "afterbegin",
+      `
+      <div class="confirm-item">
+          <div>
+            <img src=${item?.image?.thumbnail} alt=${item?.name}>
+            <div>
+              <p>${item?.name}</p>
+              <span>${item?.quantity}x<span>@${item?.price}</span></span>
+            </div>
+          </div>
+          <p>$${(item?.price * item?.quantity).toFixed(2)}</p>
+        </div>
+      `
+    );
+  });
+}
+
+overLay.addEventListener("click", (event) => {
+  if (event.target.classList.contains("overlay")) {
+    overLay.style.display = "none";
+  }
+});
+
+function resetCart() {
+  cartState = [];
+  overLay.style.display = "none";
+  emptyCart.style.display = "block";
+  neworderConfirmation.innerHTML = "";
+  totalCost.innerHTML = "";
+
+  updateCartUI();
 }
